8d42737126010c02487f8f808301cb38fd2337e6
Rafael Antonny <rafaelantonny6@gmail.com>
2023-08-30 22:13:36 -0300

    Finalizei o projeto com a validação dos inputs

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -13,30 +13,46 @@ public class ContaTerminal {
         System.out.println("Por favor digite o numero da sua conta");
 
         int numeroDaConta = input.nextInt();
+
+        if(numeroDaConta == 0) {
+            System.out.println("O numero da conta não pode ser nulo");
+            input.close();
+        }
+
         input.nextLine();
 
         System.out.println("Por favor digite a sua agência");
 
         String agencia = input.nextLine();
 
+        if (agencia.isBlank()) {
+            System.out.println("Agência não pode ser nula");
+            input.close();
+        }
+
         System.out.println("Por favor digite o seu nome");
 
         String nomeCliente = input.nextLine();
 
+        if (nomeCliente == null) {
+            System.out.println("O nome do cliente não pode ser nulo");
+            input.close();
+        }
+
         System.out.println("Por favor digite o seu saldo");
 
         double saldo = input.nextDouble();
+
         input.nextLine();
 
         ContaModel conta = new ContaModel(numeroDaConta, agencia, nomeCliente, saldo);
-
-        input.close();
 
         System.out.println("Olá " + conta.nomeCliente()
                 + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.agencia() +
                 ", conta " + conta.numeroDaConta() + " e seu saldo " + conta.saldo()
                 + " já está disponivel para o saque");
 
+        input.close();
     }
 
 }
